2bea8c74e6d75ed01769fc64c3d023ef94a117bf
Miguel Biron-Lattes <miguelbl9@gmail.com>
2025-12-02 12:05:58 -0800

    diagnostic str + more test

--- tests/test_preconditioning.py
+++ tests/test_preconditioning.py
@@ -9,8 +9,7 @@ from numpyro.infer import MCMC
 
 from automcmc.slicer import HitAndRunSliceSampler
 from automcmc.autohmc import AutoMALA
-from automcmc import utils
-from automcmc import preconditioning
+from automcmc import utils, preconditioning, selectors
 
 class TestPreconditioning(unittest.TestCase):
 
@@ -29,47 +28,67 @@ class TestPreconditioning(unittest.TestCase):
             preconditioning.FixedDiagonalPreconditioner(),
             preconditioning.FixedDensePreconditioner(),
         )
+        sels = (
+            selectors.DeterministicSymmetricSelector(),
+            selectors.MaxEJDSelector()
+        )
         for kernel_class in (AutoMALA, HitAndRunSliceSampler):
-            all_min_ess = []
-            for p in precs:
-                with self.subTest(kernel_class=kernel_class, prec_class=type(p)):
-                    kernel = kernel_class(potential_fn = pot_fn, preconditioner = p)
-                    mcmc = MCMC(kernel, num_warmup=n_warmup, num_samples=n_keep, progress_bar=False)
-                    rng_key, mcmc_key = random.split(rng_key)
-                    mcmc.run(mcmc_key, init_params=init_vals)
-                    last_base_precond_state = mcmc.last_state.base_precond_state
-                    min_ess = testutils.extremal_diagnostics(mcmc)[1]
-                    all_min_ess.append(min_ess)
-                    print(f"{type(p)}: min_ess={min_ess}")
-                    self.assertGreater(min_ess, 100)
+            for sel in sels:
+                if kernel_class is HitAndRunSliceSampler and sel != sels[0]:
+                    # Slicer doesnt actually use the selector
+                    continue
+                all_min_ess = []
+                for p in precs:
+                    subtest_config={
+                        'kernel_class': kernel_class, 
+                        'sel_class': type(sel),
+                        'prec_class': type(p)
+                    }
+                    with self.subTest(**subtest_config):
+                        kernel = kernel_class(
+                            potential_fn = pot_fn, preconditioner = p
+                        )
+                        mcmc = MCMC(
+                            kernel, 
+                            num_warmup=n_warmup, 
+                            num_samples=n_keep, 
+                            progress_bar=False
+                        )
+                        rng_key, mcmc_key = random.split(rng_key)
+                        mcmc.run(mcmc_key, init_params=init_vals)
+                        last_base_precond_state = mcmc.last_state.base_precond_state
+                        min_ess = testutils.extremal_diagnostics(mcmc)[1]
+                        all_min_ess.append(min_ess)
+                        print(f"{subtest_config}: min_ess={min_ess}")
+                        self.assertGreater(min_ess, 100)
 
-                    # check factors
-                    L = last_base_precond_state.var_tril_factor
-                    U = last_base_precond_state.inv_var_triu_factor
-                    last_round_used_var = last_base_precond_state.var
-                    if jnp.ndim(L) == 2:
-                        v = L@L.T
-                        I1, I2 = (U.T@L, jnp.identity(L.shape[-1]))
-                    else:
-                        v = L*L
-                        I1, I2 = (U*L, jnp.ones_like(L))
-                    self.assertTrue(jnp.allclose(v, last_round_used_var, rtol=tol))
-                    self.assertTrue(jnp.allclose(I1, I2, atol=tol)) # use atol for off-diag zeros
+                        # check factors
+                        L = last_base_precond_state.var_tril_factor
+                        U = last_base_precond_state.inv_var_triu_factor
+                        last_round_used_var = last_base_precond_state.var
+                        if jnp.ndim(L) == 2:
+                            v = L@L.T
+                            I1, I2 = (U.T@L, jnp.identity(L.shape[-1]))
+                        else:
+                            v = L*L
+                            I1, I2 = (U*L, jnp.ones_like(L))
+                        self.assertTrue(jnp.allclose(v, last_round_used_var, rtol=tol))
+                        self.assertTrue(jnp.allclose(I1, I2, atol=tol)) # use atol for off-diag zeros
 
-                    # check preconditioner state variance and online estimator against true var
-                    if preconditioning.is_dense(p):
-                        last_round_estimate_var = mcmc.last_state.stats.adapt_stats.sample_var
-                        self.assertTrue(jnp.allclose(S, last_round_used_var, rtol=tol))
-                        self.assertTrue(jnp.allclose(S, last_round_estimate_var, rtol=tol))
-                    else:
-                        diag_S = jnp.diag(S)
-                        last_round_estimate_var = mcmc.last_state.stats.adapt_stats.sample_var
-                        self.assertTrue(jnp.allclose(diag_S, last_round_estimate_var, rtol=tol))
-                        if not isinstance(p, preconditioning.IdentityDiagonalPreconditioner):
-                            self.assertTrue(jnp.allclose(diag_S, last_round_used_var, rtol=tol))
+                        # check preconditioner state variance and online estimator against true var
+                        if preconditioning.is_dense(p):
+                            last_round_estimate_var = mcmc.last_state.stats.adapt_stats.sample_var
+                            self.assertTrue(jnp.allclose(S, last_round_used_var, rtol=tol))
+                            self.assertTrue(jnp.allclose(S, last_round_estimate_var, rtol=tol))
+                        else:
+                            diag_S = jnp.diag(S)
+                            last_round_estimate_var = mcmc.last_state.stats.adapt_stats.sample_var
+                            self.assertTrue(jnp.allclose(diag_S, last_round_estimate_var, rtol=tol))
+                            if not isinstance(p, preconditioning.IdentityDiagonalPreconditioner):
+                                self.assertTrue(jnp.allclose(diag_S, last_round_used_var, rtol=tol))
 
-            # check that Dense gives best performance
-            self.assertEqual(all_min_ess[-1], max(all_min_ess))
+                # check that Dense gives highest ess
+                self.assertEqual(all_min_ess[-1], max(all_min_ess))
 
 if __name__ == '__main__':
     unittest.main()
